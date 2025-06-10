62a6251a7db9615df9d9595e12e44cc07b13b7ec
idgleb2 <id_gleb@mail.ru>
2025-06-10 04:56:31 -0300

    1

--- app/src/main/java/com/ursolgleb/controlparental/workers/SyncWorker.kt
+++ app/src/main/java/com/ursolgleb/controlparental/workers/SyncWorker.kt
@@ -12,6 +12,7 @@ import com.ursolgleb.controlparental.data.remote.models.toEntity
 import com.ursolgleb.controlparental.di.SyncWorkerEntryPoint
 import java.util.concurrent.TimeUnit
 import dagger.hilt.android.EntryPointAccessors
+import retrofit2.HttpException
 
 class SyncWorker(
     context: Context,
@@ -42,29 +43,33 @@ class SyncWorker(
             remoteRepo.pushHorarios(horarios)
 
 
-/*            val remoteApps = remoteRepo.fetchApps()
-            if (remoteApps.isNotEmpty()) {
-                val entities = remoteApps.mapNotNull { it.toEntity() }
-                if (entities.isNotEmpty()) {
-                    Log.e("SyncWorker", "Ejecutando doWork() insertAppsEntidades Start...")
-                    localRepo.insertAppsEntidades(entities)
-                    Log.e("SyncWorker", "Ejecutando doWork() insertAppsEntidades End...")
-                }
-            }
-            val remoteHorarios = remoteRepo.fetchHorarios()
-            if (remoteHorarios.isNotEmpty()) {
-                remoteHorarios.mapNotNull { it.toEntity() }.forEach { horario ->
-                    localRepo.addHorarioBD(horario)
-                }
-            }*/
+            /*            val remoteApps = remoteRepo.fetchApps()
+                        if (remoteApps.isNotEmpty()) {
+                            val entities = remoteApps.mapNotNull { it.toEntity() }
+                            if (entities.isNotEmpty()) {
+                                Log.e("SyncWorker", "Ejecutando doWork() insertAppsEntidades Start...")
+                                localRepo.insertAppsEntidades(entities)
+                                Log.e("SyncWorker", "Ejecutando doWork() insertAppsEntidades End...")
+                            }
+                        }
+                        val remoteHorarios = remoteRepo.fetchHorarios()
+                        if (remoteHorarios.isNotEmpty()) {
+                            remoteHorarios.mapNotNull { it.toEntity() }.forEach { horario ->
+                                localRepo.addHorarioBD(horario)
+                            }
+                        }*/
 
             //  Reprogramar el worker
             scheduleNextWork(applicationContext)
             Log.e("SyncWorker", "Ejecutando doWork() scheduleNextWork...")
             return Result.success()
 
+        } catch (e: HttpException) {
+            val body = e.response()?.errorBody()?.string()
+            Log.e("SyncWorker", "HTTP error ${'$'}{e.code()} body: ${'$'}body")
+            return Result.retry()
         } catch (e: Exception) {
-            Log.e("SyncWorker", "Error $e")
+            Log.e("SyncWorker", "Error ${'$'}e")
             return Result.retry()
         }
 
